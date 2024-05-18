049d97af7c08087af1e4b599649602a3d91c4e0c
CheonSeungHyun <cheonseunghyun@CheonSeungHyuns-MacBook-Pro.local>
2024-05-18 18:47:20 +0900

    setting: 리액트, 스프링 연동

---
+++ src/main/java/com/example/gamerecommendapp/GameRecommendAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamerecommendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRecommendAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameRecommendAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamerecommendapp/GameRecommendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamerecommendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRecommendAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
