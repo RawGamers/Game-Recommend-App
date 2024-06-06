46dcc2a53f709e823de636763db84a74adae2a35
swc9803 <swc9803@gmail.com>
2024-06-07 00:57:49 +0900

    test

--- src/main/frontend/src/common/sidebar/index.jsx
+++ src/main/frontend/src/common/sidebar/index.jsx
@@ -1,11 +1,28 @@
+import { Link } from "react-router-dom";
+import { Board } from "../../assets/svg/icon-board.jsx";
 import styles from "./index.module.scss";
 
+const menus = [
+	{ path: "/auth/sign-in", name: "인기 게임", icon: Board },
+	{ path: "/", name: "신작 게임", icon: Board },
+	{ path: "/", name: "카테고리별 게임", icon: Board },
+	{ path: "/", name: "게시판", icon: Board },
+];
 const SideBar = () => {
-    return (
-        <aside className={styles.container}>
-            <p>SideBar</p>
-        </aside>
-    );
+	return (
+		<aside className={styles.container}>
+			<ul>
+				{menus.map((menu, index) => (
+					<li key={index}>
+						<Link to={menu.path}>
+							<menu.icon />
+							{menu.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</aside>
+	);
 };
 
 export default SideBar;
