e09e3bad49e074b7a0f8f0f1827c6c8643276795
Sungwoo Choi <swc9803@gmail.com>
2024-06-23 00:06:04 +0900

    resize event 추가

--- src/main/frontend/src/app/layout.jsx
+++ src/main/frontend/src/app/layout.jsx
@@ -5,34 +5,25 @@ import Header from "../common/header/page.jsx";
 import Sidebar from "../common/sidebar/page.jsx";
 import Footer from "../common/footer/page.jsx";
 
+// event
+import ResizeHandler from "@/components/ResizeHandler";
+
 export const metadata = {
 	title: "Game Recommend App",
 	description: "Game Recommend App",
 };
 
 export default function RootLayout({ children }) {
-	// const onResize = () => {
-	// 	const vh = window.innerHeight * 0.01;
-	// 	document.documentElement.style.setProperty("--vh", `${vh}px`);
-	// };
-
-	// useEffect(() => {
-	// 	window.addEventListener("resize", onResize);
-	// 	onResize();
-	// 	return () => {
-	// 		window.removeEventListener("resize", onResize);
-	// 	};
-	// }, []);
-
 	return (
 		<html lang="ko">
 			<body>
 				<Header />
 				<Sidebar />
-				<div className="content">
-					{children}
+				<div className="layout_content">
+					<div className="view">{children}</div>
 					<Footer />
 				</div>
+				<ResizeHandler />
 			</body>
 		</html>
 	);
